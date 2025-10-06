30debc6118c7338f7121910183f185a6262211c2
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-10-06 20:56:46 +0300

    Update main.py

---
+++ task_1/main.py
@@ -0,0 +1,12 @@
+var_int = 70
+var_float = 8.4
+var_str = "No"
+
+big_int35 = var_int * 3.5
+var_float = var_float - 1
+
+print(var_int / var_float)
+print(big_int35 / var_float)
+
+var_str = "No" * 3 + "Yes" * 2
+print(var_str)
